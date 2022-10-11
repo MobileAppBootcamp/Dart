81d17367f492cb59b73a392cfd07895e837d7902
mohamedabdiahmed <mohamedahmedabdiah@hotmail.com>
2022-10-11 20:40:07 +0300

    dib u habayn ayaan ku sameeyey

--- Day1/List.dart
+++ Day1/List.dart
@@ -1,8 +1,12 @@
-// List waxay ina siinaysaa qiimayaal kala duwan balse 
+// List waxay ina siinaysaa qiimayaal kala duwan balse
 // waxay u dhaqmaysaa sidi Generics ka waxayse lamid tahay Array
 
-void main(){
-  List names = ["Ahmed",40];
+void main() {
+// List gan ayaa ah  generics.
+  List<String> myString = ["Ahmed", "Ali"];
+  print(myString);
+
+// qaabkan ayaa ah sida arrays ka
+  List names = ["Ahmed", 40];
   print(names);
 }
-
